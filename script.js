641d39c92175258acc5684c96c1670cecc3aa3ab
Mark Lape <MarkLape0828>
2025-01-30 10:29:42 +0800

    Added Todo CRUD Management project

---
+++ script.js
@@ -0,0 +1,50 @@
+// ===== Todo CRUD Management =====
+
+//Array to store todos
+
+let todos = [] ;
+
+// DOM Elements
+const todoForm = document.getElementById('todo-form');
+const todoInput = document.getElementById('todo-input');
+const todoList = document.getElementById('todo-list');
+
+//function to render todos
+function renderTodos() {
+    todoList.innerHTML = ''; //clear the list
+    todos.forEach((todo, index) => {
+        const li = document.createElement('li');
+        li.className = 'todo-item';
+        li.innerHTML = `
+            <span>${todo}</span>
+            <button onclick="editTodo(${index})">Edit</button>
+            <button onclick="deleteTodo(${index})">Delete</button> 
+        `;
+        todoList.appendChild(li);
+
+    });
+}
+
+// Fuction to edit a todo
+function editTodo(index) {
+    const updatedTodo = prompt ('Edit your todo:', todos [index]);
+    if (updatedTodo !== null){
+        todos [index] = updatedTodo.trim();
+        renderTodos();
+    }
+
+}
+
+// Fuction to delete a todo
+function deleteTodo(index){
+    if (confirm ('Are you sure you want to delete this todo?')){
+        todos.splice(index, 1);
+        renderTodos;
+    }
+}
+
+//Event Listeners
+todoForm.addEventListener('submit', addTodo);
+
+//Initial render
+renderTodos();
